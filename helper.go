d9d35b4b93f1c2bbb260cad5469e951f28d0a803
Karl Lin <linroex@coder.tw>
2022-04-05 01:23:05 +0800

    RandString rand seed

--- helper.go
+++ helper.go
@@ -1,9 +1,13 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func RandString(n int) string {
 	const letterBytes = "abcdefghijkmnorstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ0123456789"
+	rand.Seed(time.Now().UnixMicro())
 
 	b := make([]byte, n)
 	for i := range b {
